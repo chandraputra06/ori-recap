0c1f101f9332a783e9fb8b02afd2b7035322df28
chandraputra06 <chandra.wisesa06@gmail.com>
2026-05-02 19:22:50 +0800

    Fix default border color for Tailwind admin UI

--- resources/views/layouts/styles/tailwind.blade.php
+++ resources/views/layouts/styles/tailwind.blade.php
@@ -1,2 +1,2 @@
-<link rel="stylesheet" href="{{ asset('build/assets/app-DTcMM_2J.css') }}">
+<link rel="stylesheet" href="{{ asset('build/assets/app-Dd0LC7h-.css') }}">
 <script type="module" src="{{ asset('build/assets/app-BuG9aa18.js') }}"></script>
